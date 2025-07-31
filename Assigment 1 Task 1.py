862dab61ba8c3bb7c8241e4b22c5c9d18126ced8
psbanerjee <78534016+psbanerjee@users.noreply.github.com>
2025-08-01 02:10:17 +0530

    Add files via upload

---
+++ Assigment 1 Task 1.py
@@ -0,0 +1,7 @@
+x=input("Enter the first number: ")
+y=input("Enter the second number: ")
+x=int(x)
+y=int(y)
+print("\nAddition: ",x+y,"\nSubtraction: "
+      ,x-y,"\nMultiplication: ",x*y
+      ,"\nDivision: ",x/y)
